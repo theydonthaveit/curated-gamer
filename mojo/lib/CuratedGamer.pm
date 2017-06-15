76ad2dca57c0d92bd3ccf5d6572a9a38c61ae58f
Alan Williams <alan.williams@transfer-to.com>
2017-06-15 15:30:12 +0100

    built ign restful api json service

--- mojo/lib/CuratedGamer.pm
+++ mojo/lib/CuratedGamer.pm
@@ -3,21 +3,13 @@ use Mojo::Base 'Mojolicious';
 
 # This method will run once at server start
 sub startup {
-  my $self = shift;
+    my $self = shift;
+    
+    # Router
+    my $r = $self->routes;
 
-  # Documentation browser under "/perldoc"
-  $self->plugin('PODRenderer');
-
-  # Router
-  my $r = $self->routes;
-
-  # Normal route to controller
-  $r->get('/')->to('ign#home');
-  $r->get('/ign')->to('ign#home');
-  $r->get('/gamespot')->to('gamespot#home');
-  $r->get('/kotaku')->to('kotaku#home');
-  $r->get('/gamefaqs')->to('gamefaqs#home');
-  $r->get('/nfourg')->to('nfourg#home');
+    # Normal route to controller
+    $r->get('/api/V1/sites/ign/articles')->to('ign#articles');
 }
 
 1;
--- mojo/lib/process/db/Insert.pm
+++ mojo/lib/process/db/Insert.pm
@@ -2,6 +2,8 @@ package Insert;
 use strict;
 use warnings;
 
+use Project::Libs lib_dirs => [qw(mojo)];
+
 use process::db::Cleaner;
 use MongoDB;
 
@@ -10,52 +12,52 @@ use namespace::clean;
 
 has db => ( is => 'rw' );
 has collection => ( is => 'rw' );
-# has id => ( is => 'rw' );
-# has title => ( is => 'rw' );
-# has description => ( is => 'rw' );
-# has link => ( is => 'rw' );
-# has content => ( is => 'lazy' );
-# has instagram => ( is => 'rw' );
-# has twitter => ( is => 'rw' );
-# has reddit => ( is => 'rw' );
+has id => ( is => 'rw' );
+has title => ( is => 'rw' );
+has description => ( is => 'rw' );
+has link => ( is => 'rw' );
+has content => ( is => 'lazy' );
+has instagram => ( is => 'rw' );
+has twitter => ( is => 'rw' );
+has reddit => ( is => 'rw' );
 has json => ( is => 'ro' );
 
-# sub run
-# {
-#     my $self = shift;
-#     my %args = @_;
-#
-#     my $client =
-#         MongoDB->connect();
-#
-#     my $content =
-#         $args{db}
-#         .'.'
-#         .$args{collection};
-#
-#     my $collection = $client->ns($content);
-#
-#     $collection->insert_one({
-#         id => $args{id},
-#         title => $args{title},
-#         description =>
-#             Cleaner->run(
-#                 $args{description} ),
-#         link => $args{link},
-#         (
-#             defined $args{content}
-#             ? ( content =>
-#                     Cleaner->run(
-#                         $args{content}) )
-#             : ( content => undef )
-#         ),
-#         instagram => $args{instagram} // '',
-#         twitter => $args{twitter} // '',
-#         reddit => $args{reddit} // ''
-#     });
-#
-#     # ADD SOME LOGGING
-# }
+sub run
+{
+    my $self = shift;
+    my %args = @_;
+
+    my $client =
+        MongoDB->connect();
+
+    my $content =
+        $self->db
+        .'.'
+        .$self->collection;
+
+    my $collection = $client->ns($content);
+
+    $collection->insert_one({
+        id => $self->id,
+        title => $self->title,
+        description =>
+            Cleaner->run(
+                $self->description ),
+        link => $self->link,
+        (
+            defined $self->content
+            ? ( content =>
+                    Cleaner->run(
+                        $self->content) )
+            : ( content => undef )
+        ),
+        instagram => $self->instagram // '',
+        twitter => $self->twitter // '',
+        reddit => $self->reddit // ''
+    });
+
+    # ADD SOME LOGGING
+}
 
 sub steam_game_list
 {
